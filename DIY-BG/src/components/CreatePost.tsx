a2d96cec650307f18d8c294a69df4d015d6662ab
Ph1los0phy <lsimeon.minkov@gmail.com>
2025-05-10 16:38:56 +0300

    Added Home and CreatePost View component files

---
+++ DIY-BG/src/components/CreatePost.tsx
@@ -0,0 +1,7 @@
+const CreatePost = () => {
+    return (
+        <p> Create Post View</p>
+    )
+}
+
+export default CreatePost
---
+++ DIY-BG/src/components/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <p>Home view</p>
+    )
+}
+
+export default Home
