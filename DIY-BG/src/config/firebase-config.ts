c66c62d153b83db61ed5887ccf60a27ebe7189b8
Nick Pantev <103816194+IYI-Pantev@users.noreply.github.com>
2025-05-10 15:27:59 +0300

    firebase config added

---
+++ DIY-BG/src/config/firebase-config.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+// import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  //База данни
+  databaseURL: import.meta.env.VITE_FIREBASE_URL,
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// the Firebase authentication handler
+export const auth = getAuth(app);
+// the Realtime Database handler
+export const db = getDatabase(app);
+
+// const analytics = getAnalytics(app);
